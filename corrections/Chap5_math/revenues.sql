2aeda34712bae4ab9420212cafda8a9cbae2b294
PaulErnest <PaulErMartin@yahoo.fr>
2024-11-26 12:30:40 +0200

    correction fix

--- corrections/Chap5_math/revenues.sql
+++ corrections/Chap5_math/revenues.sql
@@ -2,14 +2,23 @@ CREATE TABLE revenu (
     salaire integer
 );
 
-INSERT INTO age (ages) VALUES
+INSERT INTO revenu (salaire) VALUES
 (500), (1000), (1500), (2000), (3000), (4000), (4500), (50000);
 
+--Version 1
 SELECT
-unnest(
-	percentile_cont(array[0.25, 0.5, 0.75])
-	WITHIN GROUP (ORDER BY salaire)
-	) AS "quartiles_revenus",
+	percentile_cont(0.25) WITHIN GROUP (ORDER BY salaire) AS "1er quartile",
+	percentile_cont(0.5) WITHIN GROUP (ORDER BY salaire) AS "mediane",
+	percentile_cont(0.75) WITHIN GROUP (ORDER BY salaire) AS "3e quartiles",
  	SUM(salaire) AS "total_revenus",
 	AVG(salaire) AS "avg_revenus"
-FROM revenus;
+FROM revenu;
+
+--Version 2
+SELECT
+unnest(
+	percentile_cont(array[0.25,0.5,0.75]) WITHIN GROUP (ORDER BY salaire) AS "quartiles",
+ 	SUM(salaire) AS "total_revenus",
+	AVG(salaire) AS "avg_revenus"
+)
+FROM revenu;
