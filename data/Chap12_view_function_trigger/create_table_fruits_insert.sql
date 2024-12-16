f58c6e96b4a42c31dbc596657f287f2ba51e68b7
PaulErnest <PaulErMartin@yahoo.fr>
2024-12-16 21:27:49 +0100

    add create table fruits without .csv

---
+++ data/Chap12_view_function_trigger/create_table_fruits_insert.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ventes_fruits (
+    fruit VARCHAR(50),
+    annee INT,
+    ventes INT
+);
+
+INSERT INTO ventes_fruits (fruit, annee, ventes)
+VALUES
+    ('Pomme', 2022, 150),
+    ('Pomme', 2023, 180),
+    ('Banane', 2022, 200),
+    ('Banane', 2023, 220),
+    ('Orange', 2022, 300),
+    ('Orange', 2023, 350),
+    ('Fraise', 2022, 120),
+    ('Fraise', 2023, 140);
